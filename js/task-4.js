9811bee060ab558de56109b463e5ce46f4ff68ff
Yurii Fishbakh <bunbanto@gmail.com>
2025-03-03 00:06:18 +0100

    done task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const mail = form.elements['email'].value.trim();
+  const pass = form.elements['password'].value.trim();
+
+  if (!mail || !pass) {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email: mail,
+    password: pass,
+  };
+
+  console.log(formData);
+  form.reset();
+}
