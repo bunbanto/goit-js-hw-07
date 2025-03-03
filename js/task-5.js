bbabe36aaae3de9901cb33e93e32cfa2f8632e72
Yurii Fishbakh <bunbanto@gmail.com>
2025-03-03 12:24:28 +0100

    done task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+const output = document.querySelector('span.color');
+
+button.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  output.textContent = newColor;
+});
