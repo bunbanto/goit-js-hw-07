ff2d7e5435a677d4ef25c4fedb2fbc61ce89a8d8
Yurii Fishbakh <bunbanto@gmail.com>
2025-03-03 17:38:59 +0100

    done task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,39 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.querySelector('#controls');
+const input = controls.querySelector('input');
+const createBtn = controls.querySelector('[data-create]');
+const destroyBtn = controls.querySelector('[data-destroy]');
+const outputBoxes = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', () => {
+  const amount = Number(input.value);
+  if (amount < 1 || amount > 100) return;
+  createBoxes(amount);
+  input.value = '';
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  destroyBoxes();
+  const fragment = document.createDocumentFragment();
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(div);
+    size += 10;
+  }
+
+  outputBoxes.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  outputBoxes.innerHTML = '';
+}
